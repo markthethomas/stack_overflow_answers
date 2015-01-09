6c861b0776c1aadfc44ec02be416e4297e914644
Mark Thomas <markthethomas@gmail.com>
2015-01-09 09:56:56 -0800

    Added some more info to js_regex

--- js_regex/program.js
+++ js_regex/program.js
@@ -1,11 +1,9 @@
-var foo = '123asfasff111f6';
+//  Original question: http://stackoverflow.com/questions/27865682/effective-solution-for-create-array-containing-all-groups-in-regex-matches/27866227#27866227
 
+// User wanted a way to search a string or strings and return a matched set of results as an array
 
 function findInString(string, pattern) {
   return string.split('').filter(function (element) {
      return element.match(pattern)
   })
 }
-
-
-console.log(findInString(foo, /\d/g));
